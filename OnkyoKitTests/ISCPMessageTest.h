e109f104fd91f7d336bedbb9bab7d54d487b9348
Jeff Hutchison <jeff@jeffhutchison.com>
2013-06-11 19:25:22 -0400

    Revert to SenTest unit tests.

---
+++ OnkyoKitTests/ISCPMessageTest.h
@@ -0,0 +1,13 @@
+//
+//  ISCPMessageTest.h
+//  OnkyoKit
+//
+//  Created by Jeff Hutchison on 6/11/13.
+//  Copyright (c) 2013 Jeff Hutchison. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ISCPMessageTest : SenTestCase
+
+@end
---
+++ OnkyoKitTests/ISCPMessageTest.m
@@ -0,0 +1,50 @@
+//
+//  ISCPMessageTest.m
+//  OnkyoKit
+//
+//  Created by Jeff Hutchison on 6/11/13.
+//  Copyright (c) 2013 Jeff Hutchison. All rights reserved.
+//
+
+#import "ISCPMessageTest.h"
+#import <OnkyoKit/ISCPMessage.h>
+
+@implementation ISCPMessageTest
+
+- (void)testInitWithDataTestsParameter {
+    ISCPMessage *message;
+    STAssertThrowsSpecificNamed(message = [[ISCPMessage alloc] initWithData:nil], NSException,
+                                NSInternalInconsistencyException, @"should through exception for nil paramter");
+}
+
+- (void)testInitWithMessageTestsParameter {
+    ISCPMessage *message;
+    STAssertThrowsSpecificNamed(message = [[ISCPMessage alloc] initWithMessage:nil], NSException,
+                                NSInternalInconsistencyException, @"should through exception for nil paramter");
+}
+
+- (void)testConvertDataDelimitedByEOF {
+    NSString *msg = @"PWR01";
+    ISCPMessage *iscp = [[ISCPMessage alloc] initWithData:[[NSString stringWithFormat:@"!1%@\x1a", msg] dataUsingEncoding:NSASCIIStringEncoding]];
+    STAssertEqualObjects(iscp.message, msg, @"should convert data delimeted by EOF");
+}
+
+- (void)testConvertDataDelimitedByEOF_CR {
+    NSString *msg = @"PWR01";
+    ISCPMessage *iscp = [[ISCPMessage alloc] initWithData:[[NSString stringWithFormat:@"!1%@\x1a\r", msg] dataUsingEncoding:NSASCIIStringEncoding]];
+    STAssertEqualObjects(iscp.message, msg, @"should convert data delimeted by EOF");
+}
+
+- (void)testConvertDataDelimitedByEOF_CR_LF {
+    NSString *msg = @"PWR01";
+    ISCPMessage *iscp = [[ISCPMessage alloc] initWithData:[[NSString stringWithFormat:@"!1%@\x1a\r\x0a", msg] dataUsingEncoding:NSASCIIStringEncoding]];
+    STAssertEqualObjects(iscp.message, msg, @"should convert data delimeted by EOF");
+}
+
+- (void)testConvertMessageToData {
+    NSString *msg = @"NLTF300001100120000FFFF00NE";
+    ISCPMessage *iscp = [[ISCPMessage alloc] initWithMessage:msg];
+    STAssertEqualObjects(iscp.data, ([NSString stringWithFormat:@"!1%@\r", msg]), @"should convert message to data");
+}
+
+@end
