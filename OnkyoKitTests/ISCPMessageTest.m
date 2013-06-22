5ff8b61af0fb50cf4d2a8ed772fc1a34cb18411d
Jeff Hutchison <jeff@jeffhutchison.com>
2013-06-22 18:42:00 -0400

    Remove flaky tests for exceptions.

--- OnkyoKitTests/ISCPMessageTest.m
+++ OnkyoKitTests/ISCPMessageTest.m
@@ -11,18 +11,6 @@
 
 @implementation ISCPMessageTest
 
-- (void)testInitWithNilData {
-    ISCPMessage *message;
-    XCTAssertThrowsSpecificNamed(message = [[ISCPMessage alloc] initWithData:nil], NSException,
-                                NSInternalInconsistencyException, @"should through exception for nil paramter");
-}
-
-- (void)testInitWithNilMessage {
-    ISCPMessage *message;
-    XCTAssertThrowsSpecificNamed(message = [[ISCPMessage alloc] initWithMessage:nil], NSException,
-                                NSInternalInconsistencyException, @"should through exception for nil paramter");
-}
-
 - (void)testConvertDataDelimitedByEOF {
     NSString *msg = @"PWR01";
     ISCPMessage *iscp = [[ISCPMessage alloc] initWithData:[[NSString stringWithFormat:@"!1%@\x1a", msg] dataUsingEncoding:NSASCIIStringEncoding]];
