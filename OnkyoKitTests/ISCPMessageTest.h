37d284ce54a44a3ba7ae236f8fd6c17133b53952
Jeff Hutchison <jeff@jeffhutchison.com>
2013-06-22 07:35:44 -0400

    Convert to Xcode 5 XCTest, remove ocmock.

--- OnkyoKitTests/ISCPMessageTest.h
+++ OnkyoKitTests/ISCPMessageTest.h
@@ -6,8 +6,8 @@
 //  Copyright (c) 2013 Jeff Hutchison. All rights reserved.
 //
 
-#import <SenTestingKit/SenTestingKit.h>
+#import <XCTest/XCTest.h>
 
-@interface ISCPMessageTest : SenTestCase
+@interface ISCPMessageTest : XCTestCase
 
 @end
