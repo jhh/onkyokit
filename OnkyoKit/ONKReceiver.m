080c9c16fe0c5d733a3b7b4582b753e838fb9310
Jeff Hutchison <jeff@jeffhutchison.com>
2013-07-19 19:25:45 -0400

    Low water was set arbitrarily to 5.

--- OnkyoKit/ONKReceiver.m
+++ OnkyoKit/ONKReceiver.m
@@ -50,7 +50,7 @@ NSString *const ONKReceiverWasDiscoveredNotification = @"ONKReceiverWasDiscovere
         return;
     }
     _channel = dispatch_io_create(DISPATCH_IO_STREAM, fd, _socketQueue, NULL);
-    dispatch_io_set_low_water(_channel, 5);
+    dispatch_io_set_low_water(_channel, 1);
 
     dispatch_io_read(_channel, 0, SIZE_MAX, _socketQueue, ^(bool done, dispatch_data_t data, int error) {
         if(data) {
